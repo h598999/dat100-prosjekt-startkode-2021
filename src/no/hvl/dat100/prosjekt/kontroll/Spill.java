88f3e993cdd6402ae8b580409db953509d3c31a6
h598999 <89342804+h598999@users.noreply.github.com>
2021-10-18 20:26:40 +0200

    Update Spill.java

--- src/no/hvl/dat100/prosjekt/kontroll/Spill.java
+++ src/no/hvl/dat100/prosjekt/kontroll/Spill.java
@@ -1,151 +1,260 @@
 package no.hvl.dat100.prosjekt.kontroll;
 
+import java.util.ArrayList;
+
 import no.hvl.dat100.prosjekt.modell.KortSamling;
-import no.hvl.dat100.prosjekt.modell.KortUtils;
 import no.hvl.dat100.prosjekt.TODO;
+import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
+import no.hvl.dat100.prosjekt.kontroll.spill.Handling;
+import no.hvl.dat100.prosjekt.kontroll.spill.HandlingsType;
 import no.hvl.dat100.prosjekt.kontroll.spill.Spillere;
 import no.hvl.dat100.prosjekt.modell.Kort;
+import no.hvl.dat100.prosjekt.modell.KortUtils;
 
 /**
- * Abstrakt klasse som implementerer alle metodene i kontrakten (interfacet) ISpiller,
- * bortsett fra nesteHandling(). Dette er grunnen til at klassen er abstrakt.
- * For ÃƒÂ¥ lage "virkelige" spillere, mÃƒÂ¥ vi arve fra denne klassen og implementere
- * nesteHandling (fra ISpiller).
- * 
- * Klassen har objektvariablene hand (Hand), antalltrekk (heltall) og spiller
- * (Spillere). Den har to konstruktÃƒÂ¸rer. Se beskrivelse av disse.
+ * Klassen har objektvariaber som er referanser til de spillerne, nord og syd
+ * (type ISpiller). Den har ogsÃƒâ€š en bunke man deler/trekker fra og en bunke man
+ * spiller til.
  * 
  */
-public abstract class Spiller implements ISpiller {
+public class Spill {
 
-	// hand for spilleren (samling)
-	private KortSamling hand; 
+	private ISpiller nord;
+	private ISpiller syd;
 	
-	// antall trekk spilleren har gjort fra fra-bunken
-	private int antalltrekk; 
+	private Bord bord;
 	
-	// hvem spilleren er (Nord,Syd,Ingen) - se oppramsklassen Spillere
-	private Spillere spiller;
-
-	/**
-	 * Standard konstruktÃƒÂ¸r som oppretter en Spiller med en hÃƒÂ¥nd uten kort,
-	 * antalltrekk som 0 og setter spiller til Spillere.INGEN.
-	 */
-	public Spiller() {
+	// antall kort som skal deles ut til hver spiller ved start
+	public final static int ANTALL_KORT_START = Regler.ANTALL_KORT_START;
+	
+	public Spill() {
 		
 		// TODO - START
-	spiller = Spillere.INGEN; 
-	hand = new KortSamling();
-	antalltrekk = 0;
+		bord = new Bord();
+		nord = new NordSpiller(Spillere.NORD);
+		syd = new SydSpiller(Spillere.SYD);
+		
+		
+		// TODO - END
+		
+	}
 	
+	/**
+	 * Gir referanse/peker til bord.
+	 * 
+	 * @return referanse/peker bord objekt.
+	 */
+	public Bord getBord() {
+		
+		// TODO - START
+		return bord;
+
+		// TODO - END
+		
+	}
+	
+	/**
+	 * Gir referanse/peker til syd spilleren.
+	 * 
+	 * @return referanse/peker til syd spiller.
+	 */
+	public ISpiller getSyd() {
+		
+		// TODO - START
+		
+		return syd;
+
+		// TODO - END
+		
+	}
+
+	/**
+	 * Gir referanse/peker til nord.
+	 * 
+	 * @return referanse/peker til nord.
+	 */
+	public ISpiller getNord() {
+		
+		// TODO - START
+
+	return nord;
+
 		// TODO - END
 	}
 
 	/**
-	 * KonstruktÃƒÂ¸r der vi kan sette hvilken spiller det er (NORD, SYD eller
-	 * INGEN).
+	 * Metoden oppretter to spillere, nord og syd. Det opprettes to bunker, fra
+	 * og til. Alle kortene legges til fra. Bunken fra stokkes. Deretter deles
+	 * det ut kort fra fra-bunken til nord og syd i henhold til regler. Til
+	 * slutt tas ÃƒÂ¸verste kortet fra fra-bunken og legges til til-bunken.
+	 * 
+	 * Nord har type RandomSpiller (som er forhÃƒÂ¥ndefinert). Syd vil vÃƒÂ¦re spiller
+	 * av en klasse laget av gruppen (implementeres i oppgave 3).
+	 */
+	public void start() {
+		
+		// TODO - START
+	ISpiller spillernord = nord;
+	ISpiller spillersyd = syd;
+	Bord bordspill = bord;
+	delutKort();
+	Kort v = bordspill.getBunkeFra().taSiste();
+	bordspill.getBunkeTil().leggTil(v);
+		
+		// TODO - END
+	}
+
+	/**
+	 * Deler ut kort til nord og syd.
+	 * 
+	 */
+	private void delutKort() {
+
+		// TODO - START
+	
+	KortSamling kortstokk = bord.getBunkeFra();
+	KortUtils.stokk(kortstokk);
+	for (int i = 0; i<Regler.ANTALL_KORT_START; i++) {
+		Kort v = kortstokk.taSiste();
+		nord.leggTilKort(v);
+	}
+	
+	for (int i = 0; i<Regler.ANTALL_KORT_START; i++) {
+		Kort v = kortstokk.taSiste();
+		syd.leggTilKort(v);
+	}
+		// TODO - END
+	}
+
+	/**
+	 * Trekker et kort fra fra-bunken til spilleren gitt som parameter. Om
+	 * fra-bunken er tom, mÃƒÂ¥ man "snu" til-bunken. Se info om metoden
+	 * snuTilBunken().
 	 * 
 	 * @param spiller
-	 *            hvilken spiller det er.
+	 *            spilleren som trekker.
+	 * 
+	 * @return kortet som trekkes.
 	 */
-	public Spiller(Spillere spiller) {
-		this.spiller=spiller;
-		hand = new KortSamling();
-		antalltrekk = 0;
-		
-		
-		}
-	
-	
-		// TODO - END
-	
+	public Kort trekkFraBunke(ISpiller spiller) {
 
-	public int getAntallKort() {
-		
 		// TODO - START
+		Kort v = null;
+				
+				bord.snuTilBunken();
+				v = bord.getBunkeFra().taSiste();
+				spiller.leggTilKort(v);
+				int x = spiller.getAntallTrekk();
+				spiller.setAntallTrekk(x+1);
+				return v;
 		
-	return hand.getAntalKort();
+
 
 		// TODO - END
 	}
 
-	public KortSamling getHand() {
+	/**
+	 * Gir neste handling for en spiller (spilt et kort, trekker et kort, forbi)
+	 * 
+	 * @param spiller
+	 *            spiller som handle.
+	 * 
+	 * @return handlingen som blir utfÃƒÂ¸rt.
+	 */
+	public Handling nesteHandling(ISpiller spiller) {
 		
 		// TODO - START
-		
-		return hand;
-
-		// TODO - END
-	}
-
-	public int getAntallTrekk() {
-		
-		// TODO - START
-	return antalltrekk;
-
-		// TODO - END
-	}
-
-	public Spillere hvem() {
-		
-		// TODO - START
-		
-		return spiller;
+	return spiller.nesteHandling(bord.seOversteBunkeTil());
 
 		// TODO - END
 		
 	}
 
-	public void setAntallTrekk(int t) {
+	/**
+	 * Metoden spiller et kort. Den sjekker at spiller har kortet. Dersom det er
+	 * tilfelle, fjernes kortet fra spilleren og legges til til-bunken. Metoden
+	 * nulltiller ogsÃƒÂ¥ antall ganger spilleren har trukket kort.
+	 * 
+	 * @param spiller
+	 *            den som spiller.
+	 * @param kort
+	 *            kort som spilles.
+	 * 
+	 * @return true dersom spilleren har kortet, false ellers.
+	 */
+	public boolean leggnedKort(ISpiller spiller, Kort kort) {
 		
 		// TODO - START
-		
-		antalltrekk = t;
-		// TODO - END
-	}
-
-	public boolean erFerdig() {
-		
-		// TODO - START
-	boolean a = false;
-	if (hand.erTom()==true)
+		boolean a = false;
+	if (spiller.getHand().har(kort)==true) {
 		a=true;
+		spiller.getHand().fjern(kort);
+		bord.leggNedBunkeTil(kort);
+		spiller.setAntallTrekk(0);
+	}
 	return a;
-		// TODO - END
-		
-	}
+	
 
-	public void leggTilKort(Kort kort) {
-		
-		// TODO - START
-	hand.leggTil(kort);
-		// TODO - END
-		
-	}
-
-	public void fjernKort(Kort kort) {
-		
-		// TODO - START
-		hand.fjern(kort);
-		
-		// TODO - END
-		
-	}
-
-	public void fjernAlleKort() {
-		
-		// TODO - START
-		
-	hand.fjernAlle();
 		// TODO - END
 	}
 
-	public void trekker(Kort kort) {
+	/**
+	 * Metode for ÃƒÂ¥ si forbi. MÃƒÂ¥ nullstille antall ganger spilleren har trukket
+	 * kort.
+	 * 
+	 * @param spiller
+	 *            spilleren som er i tur.
+	 */
+	public void forbiSpiller(ISpiller spiller) {
 		
 		// TODO - START
-		hand.leggTil(kort);
-		antalltrekk++;
+	Handling forbi = new Handling(HandlingsType.FORBI, null);
+	forbi.skifteTur();
+	spiller.setAntallTrekk(0);
 		// TODO - END
-		
 	}
+
+	/**
+	 * Metode for ÃƒÂ¥ utfÃƒÂ¸re en handling (trekke, spille, forbi). Dersom handling
+	 * er kort, blir kortet ogsÃƒÂ¥ spilt.
+	 * 
+	 * @param spiller
+	 *            spiller som utfÃƒÂ¸rer handlingen.
+	 * @param handling
+	 *            handling som utfÃƒÂ¸res.
+	 * 
+	 * @return kort som trekkes, kort som spilles eller null ved forbi.
+	 */
+	public Kort utforHandling(ISpiller spiller, Handling handling) {
+
+		// TODO - START
+		Kort kort = null;
+
+		// Hint: del opp i de tre mulige handlinger og vurder 
+		// om noen andre private metoder i klassen kan brukes
+		// til ÃƒÂ¥ implementere denne metoden
+				
+
+if (handling.getType()==HandlingsType.FORBI) {
+ kort = null;
+	forbiSpiller(spiller);
 }
+		
+	
+if (handling.getType()==HandlingsType.TREKK) {
+	kort = bord.getBunkeFra().taSiste();
+	spiller.getHand().leggTil(kort);}
+
+if (handling.getType()==HandlingsType.LEGGNED) {
+	Kort[] hand = spiller.getHand().getSamling();
+	kort = hand[0];
+	spiller.getHand().fjern(kort);
+	bord.getBunkeTil().leggTil(kort);}
+return kort;	
+	
+}
+
+		// TODO - END
+	}
+
+
